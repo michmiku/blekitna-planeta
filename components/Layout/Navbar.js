03ac1835a5ccdf2f896a120ca632a87c6e187a1a
Mateusz Kleszcz <kleeeszcz@gmail.com>
2021-04-28 12:36:53 +0200

    added mobile navbar

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,141 +1,161 @@
-import Link from 'next/link';
 import Image from 'next/image';
 import styles from 'styles/Home.module.scss';
 import NavigationButton from './NavigationButton';
+import { faList } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useRef } from 'react';
 
 const Navbar = () => {
+
+    const linksRef = useRef(null)
+
+    const handleToggleMenu = e => {
+        if (linksRef.current.style.display === 'none' || linksRef.current.style.display === '')
+            linksRef.current.style.display = 'flex'
+        else
+            linksRef.current.style.display = 'none'
+    }
+
     return (
         <nav className={styles.mainNav}>
             <div className={styles.logo}>
+                <div className={styles.mobileMenuButton} onClick={handleToggleMenu}>
+                    <FontAwesomeIcon className={styles.dropdownIcon} icon={faList} />
+                </div>
                 <p>Błękitna Planeta</p>
                 <a className={styles.logo}>
                     <Image src='/logo.png' alt='logo' width='40' height='40' />
                 </a>
             </div>
-            <NavigationButton
-                parentLink=''
-                link=''
-                content='O nas'
-                isAside={false}
-                dropdowns={[]}
-            />
-            <NavigationButton
-                parentLink=''
-                link='aktualnosci'
-                content='Aktualności'
-                isAside={false}
-                dropdowns={[]}
-            />
-            <NavigationButton
-                parentLink=''
-                link='porady'
-                content='Porady'
-                isAside={false}
-                dropdowns={[
-                    {
-                        link: 'kacik-gromerski',
-                        content: 'Kącik gromerski',
-                        isAside: true,
-                        dropdowns: []
-                    },
-                    {
-                        link: 'okiem-akwarysty',
-                        content: 'Okiem akwarysty',
-                        isAside: true,
-                        dropdowns: []
-                    },
-                    {
-                        link: 'nasi-pupile',
-                        content: 'Nasi Pupile',
-                        isAside: true,
-                        dropdowns: [
-                            {
-                                link: 'pies',
-                                content: 'Pies',
-                                isAside: true,
-                                dropdowns: []
-                            },
-                            {
-                                link: 'kot',
-                                content: 'Kot',
-                                isAside: true,
-                                dropdowns: []
-                            },
-                            {
-                                link: 'inne',
-                                content: 'Inne',
-                                isAside: true,
-                                dropdowns: []
-                            },
-                        ]
-                    },
-                ]}
-            />
-            <NavigationButton
-                parentLink=''
-                link='nasze-realizacje'
-                content='Nasze Realizacje'
-                isAside={false}
-                dropdowns={[
-                    {
-                        link: 'akwaria-morskie',
-                        content: 'Akwaria morskie',
-                        isAside: true,
-                        dropdowns: []
-                    },
-                    {
-                        link: 'akwaria-slodkowodne',
-                        content: 'Akwaria słodkowodne',
-                        isAside: true,
-                        dropdowns: [
-                            {
-                                link: 'rybuoku',
-                                content: 'Realizacja w stylu Rybuoku',
-                                isAside: true,
-                                dropdowns: []
-                            },
-                            {
-                                link: 'slodkowodne',
-                                content: 'Nasze akwaria słodkowodne',
-                                isAside: true,
-                                dropdowns: []
-                            },
-                        ]
-                    },
-                    {
-                        link: 'terraria',
-                        content: 'Terraria',
-                        isAside: true,
-                        dropdowns: [
-                            {
-                                link: 'rozne',
-                                content: 'Różne gatunki',
-                                isAside: true,
-                                dropdowns: []
-                            },
-                            {
-                                link: 'tropikalne',
-                                content: 'Terraria tropikalne',
-                                isAside: true,
-                                dropdowns: []
-                            },
-                            {
-                                link: 'pustynne',
-                                content: 'Terraria pustynne',
-                                isAside: true,
-                                dropdowns: []
-                            },
-                        ]
-                    },
-                ]}
-            />
-            <NavigationButton
-                parentLink=''
-                link='kontakt'
-                content='Kontakt'
-                isAside={false}
-                dropdowns={[]}
-            />
+            <div
+                className={styles.allLinks}
+                ref={linksRef}
+            >
+                <NavigationButton
+                    parentLink=''
+                    link=''
+                    content='O nas'
+                    isAside={false}
+                    dropdowns={[]}
+                />
+                <NavigationButton
+                    parentLink=''
+                    link='aktualnosci'
+                    content='Aktualności'
+                    isAside={false}
+                    dropdowns={[]}
+                />
+                <NavigationButton
+                    parentLink=''
+                    link='porady'
+                    content='Porady'
+                    isAside={false}
+                    dropdowns={[
+                        {
+                            link: 'kacik-gromerski',
+                            content: 'Kącik gromerski',
+                            isAside: true,
+                            dropdowns: []
+                        },
+                        {
+                            link: 'okiem-akwarysty',
+                            content: 'Okiem akwarysty',
+                            isAside: true,
+                            dropdowns: []
+                        },
+                        {
+                            link: 'nasi-pupile',
+                            content: 'Nasi Pupile',
+                            isAside: true,
+                            dropdowns: [
+                                {
+                                    link: 'pies',
+                                    content: 'Pies',
+                                    isAside: true,
+                                    dropdowns: []
+                                },
+                                {
+                                    link: 'kot',
+                                    content: 'Kot',
+                                    isAside: true,
+                                    dropdowns: []
+                                },
+                                {
+                                    link: 'inne',
+                                    content: 'Inne',
+                                    isAside: true,
+                                    dropdowns: []
+                                },
+                            ]
+                        },
+                    ]}
+                />
+                <NavigationButton
+                    parentLink=''
+                    link='nasze-realizacje'
+                    content='Nasze Realizacje'
+                    isAside={false}
+                    dropdowns={[
+                        {
+                            link: 'akwaria-morskie',
+                            content: 'Akwaria morskie',
+                            isAside: true,
+                            dropdowns: []
+                        },
+                        {
+                            link: 'akwaria-slodkowodne',
+                            content: 'Akwaria słodkowodne',
+                            isAside: true,
+                            dropdowns: [
+                                {
+                                    link: 'rybuoku',
+                                    content: 'Realizacja w stylu Rybuoku',
+                                    isAside: true,
+                                    dropdowns: []
+                                },
+                                {
+                                    link: 'slodkowodne',
+                                    content: 'Nasze akwaria słodkowodne',
+                                    isAside: true,
+                                    dropdowns: []
+                                },
+                            ]
+                        },
+                        {
+                            link: 'terraria',
+                            content: 'Terraria',
+                            isAside: true,
+                            dropdowns: [
+                                {
+                                    link: 'rozne',
+                                    content: 'Różne gatunki',
+                                    isAside: true,
+                                    dropdowns: []
+                                },
+                                {
+                                    link: 'tropikalne',
+                                    content: 'Terraria tropikalne',
+                                    isAside: true,
+                                    dropdowns: []
+                                },
+                                {
+                                    link: 'pustynne',
+                                    content: 'Terraria pustynne',
+                                    isAside: true,
+                                    dropdowns: []
+                                },
+                            ]
+                        },
+                    ]}
+                />
+                <NavigationButton
+                    parentLink=''
+                    link='kontakt'
+                    content='Kontakt'
+                    isAside={false}
+                    dropdowns={[]}
+                />
+            </div>
         </nav>
     );
 };
--- components/Layout/NavigationButton.js
+++ components/Layout/NavigationButton.js
@@ -1,17 +1,43 @@
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
+import { useRef, useState } from 'react';
 import styles from 'styles/Home.module.scss';
 
 const NavigationButton = ({ parentLink, link, content, isAside, dropdowns }) => {
 
+    const [isToggled, setIsToggled] = useState(false)
+
+    const dropdownContentRef = useRef(null)
+
     const fullLink = `${parentLink}/${link}`
     const dropdownsLink = dropdowns.map((item, index) => <NavigationButton parentLink={fullLink} {...item} key={index} />)
+
+    const handleToggleDropdown = e => {
+        if (dropdownContentRef.current.style.display === 'none' || dropdownContentRef.current.style.display === '') {
+            dropdownContentRef.current.style.display = 'flex'
+            setIsToggled(true)
+        } else {
+            dropdownContentRef.current.style.display = 'none'
+            setIsToggled(false)
+        }
+    }
 
     return (
         dropdowns.length !== 0 ? (
             <div className={styles.linkContainer}>
                 <div className={styles.dropdown}>
-                    <a className={styles.navBtn}>{content}</a>
-                    <div className={`${styles.dropdownContent} ${isAside ? styles.aside : ''}`}>
+                    <a
+                        className={styles.navBtn}
+                        onClick={handleToggleDropdown}
+                    >
+                        {content}
+                        <FontAwesomeIcon className={styles.dropdownIcon} icon={isToggled ? faChevronUp : faChevronDown} />
+                    </a>
+                    <div
+                        className={`${styles.dropdownContent} ${isAside ? styles.aside : ''}`}
+                        ref={dropdownContentRef}
+                    >
                         {dropdowns.length != 0 && dropdownsLink}
                     </div>
                 </div>
