8bf25a28719bfad927bdc72f6e3891e743c994d2
Mateusz Kleszcz <kleeeszcz@gmail.com>
2021-05-24 00:47:25 +0200

    added mobile contact page

--- components/Contact/ContactContainer.js
+++ components/Contact/ContactContainer.js
@@ -6,6 +6,25 @@ const ContactContainer = () => {
     return (
         <>
             <h1 className={styles.pageHeader} >Skontaktuj się z nami!</h1>
+            <div className={styles.contactInformations}>
+                <div>
+                    <h1>Godziny otwarcia:</h1>
+                    <hr />
+                    <p>Poniedziałek - sobota: 10:00 - 20:00</p>
+                    <p>Niedziela: nieczynne</p>
+                </div>
+                <div>
+                    <h1>Telefon:</h1>
+                    <hr />
+                    <p>12 658 50 77</p>
+                </div>
+            </div>
+            <h1 className={styles.cityHeader}>Miasto 1</h1>
+            <div className={styles.contactContainer}>
+                <ContactMap />
+                <InformationsPanel />
+            </div>
+            <h1 className={styles.cityHeader}>Miasto 2</h1>
             <div className={styles.contactContainer}>
                 <ContactMap />
                 <InformationsPanel />
