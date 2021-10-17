ca24209b8a2682cf6e7c4e898e9f762f9f821a4e
michmiku <michmiku@gmail.com>
2021-10-17 15:53:07 +0200

    fixed build error

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -10,12 +10,13 @@ const prettier = require("prettier");
   const prettierConfig = await prettier.resolveConfig("./.prettierrc.js");
 
   // Ignore Next.js specific files (e.g., _app.js) and API routes.
-  const pages = await globby([
-    "pages/**/*{.js,.mdx}",
-    "!pages/_*.js",
-    "!pages/api",
-  ]);
-  const sitemap = `
+  try {
+    const pages = await globby([
+      "pages/**/*{.js,.mdx}",
+      "!pages/_*.js",
+      "!pages/api",
+    ]);
+    const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             ${pages
@@ -55,11 +56,13 @@ const prettier = require("prettier");
         </urlset>
     `;
 
-  // If you're not using Prettier, you can remove this.
-  const formatted = prettier.format(sitemap, {
-    ...prettierConfig,
-    parser: "html",
-  });
-
-  fs.writeFileSync("public/sitemap.xml", formatted);
+    // If you're not using Prettier, you can remove this.
+    const formatted = prettier.format(sitemap, {
+      ...prettierConfig,
+      parser: "html",
+    });
+    fs.writeFileSync("public/sitemap.xml", formatted);
+  } catch (error) {
+    console.log(error);
+  }
 })();
